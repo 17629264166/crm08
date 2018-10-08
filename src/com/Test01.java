9c16def23e0646b65f18ed9382638e98f4cdcec5
17629264166 <33753522+17629264166@users.noreply.github.com>
2018-10-08 16:35:27 +0800

    github update
    github update

--- src/com/Test01.java
+++ src/com/Test01.java
@@ -6,6 +6,7 @@ public class Test01 {
 		// TODO Auto-generated method stub
 		System.out.println("hello,github·þÎñÆ÷");
 		System.out.println("ok,github服务器 return 01");
+		System.out.println("github服务器 update 01");
 
 	}
 
